7a6e2bd44d957e8ec12917cc221c5b1851efe54a
문슬기 <tmfrlghtjr@gmail.com>
2022-01-14 23:29:05 +0900

    0114 upload

---
+++ algo_study/오리/12933.py
@@ -0,0 +1,31 @@
+import sys
+sys.stdin = open('input.txt')
+
+
+def find_duck(x):
+    for i in range(x, len(temp)):
+
+
+
+
+
+sound = input()
+
+temp = list(sound)
+# print(temp)
+duck = ['q', 'u', 'a', 'c', 'k']
+
+visited = [0] + [0] * len(temp)
+cnt = 0
+
+if len(temp) < 5:
+    print(-1)
+else:
+    for i in range(len(temp)):
+        pass
+
+
+
+"""
+quqcaukqauackck
+"""
