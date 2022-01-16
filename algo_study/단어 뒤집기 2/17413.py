2ddb66d41625cab8633a6f2c2f75b8bc9492804a
문슬기 <tmfrlghtjr@gmail.com>
2022-01-17 00:04:37 +0900

    0117 upload

--- algo_study/단어 뒤집기 2/17413.py
+++ algo_study/단어 뒤집기 2/17413.py
@@ -1,12 +1,41 @@
 import sys
 sys.stdin = open('input.txt')
 
-word = input()
+word = list(input())
 # print(word)
 
 temp = []
 stack = []
-# for i in range(len(word)):
-#     if word[i] != '<':
-#         temp.append(word[i][::-1])
+# print(word[-1])
+
+# while word:
+#     t = word.pop(0)
+#     if t == '<':
+#         temp.append(t)
+#     print(t)
 # print(temp)
+
+for i in range(len(word)):
+    if word[i] == '<':
+        if len(temp) != 0:
+            stack.append(temp)
+            temp = ''
+        temp += '<'
+        # stack.append(temp[:-1])
+    elif word[i] == '>':
+        temp += '>'
+        stack.append(temp)
+        temp = ''
+    else:
+        if word[i] != ' ':
+            temp += word[i]
+
+        if word[i] == ' ':
+            temp += ' '
+            stack.append(temp)
+            temp = ''
+        if i == len(word)-1:
+            stack.append(temp[::-1])
+            temp = ''
+
+print(*stack, sep='')
