2d0a60c62edf74382daf54b9a22bb9f61fa377d4
문슬기 <tmfrlghtjr@gmail.com>
2022-01-16 22:26:25 +0900

    0116 upload

---
+++ algo_study/단어 뒤집기 2/17413.py
@@ -0,0 +1,12 @@
+import sys
+sys.stdin = open('input.txt')
+
+word = input()
+# print(word)
+
+temp = []
+stack = []
+# for i in range(len(word)):
+#     if word[i] != '<':
+#         temp.append(word[i][::-1])
+# print(temp)
