45920d14157aa94185ad61acdf2c4b1594799ede
문슬기 <tmfrlghtjr@gmail.com>
2022-01-18 00:14:25 +0900

    0118 upload

--- algo_study/단어 뒤집기 2/17413.py
+++ algo_study/단어 뒤집기 2/17413.py
@@ -1,41 +1,50 @@
 import sys
 sys.stdin = open('input.txt')
+from collections import deque
 
 word = list(input())
 # print(word)
+q = deque(word)
 
-temp = []
+temp = ''
 stack = []
-# print(word[-1])
 
-# while word:
-#     t = word.pop(0)
-#     if t == '<':
-#         temp.append(t)
-#     print(t)
-# print(temp)
+flag = 0
 
-for i in range(len(word)):
-    if word[i] == '<':
+while q:
+    t = q.popleft()
+    # < 있으면 안뒤집게
+
+    if t == '<':
         if len(temp) != 0:
-            stack.append(temp)
+            print(temp[::-1], end='')
             temp = ''
-        temp += '<'
-        # stack.append(temp[:-1])
-    elif word[i] == '>':
-        temp += '>'
-        stack.append(temp)
+        # temp += t
+        print(t, end='')
+        flag = 1
+        continue
+        # > 만나면 상태 원래대로
+    if t == '>' and flag == 1:
+        flag = 0
+        temp += t
+        print(temp, end='')
         temp = ''
+        continue
+    if t == ' ':
+        if flag == 1:
+            print(temp, end=' ')
+            temp = ''
+        else:
+            print(temp[::-1], end=' ')
+            temp = ''
+            continue
     else:
-        if word[i] != ' ':
-            temp += word[i]
+        temp += t
 
-        if word[i] == ' ':
-            temp += ' '
-            stack.append(temp)
-            temp = ''
-        if i == len(word)-1:
-            stack.append(temp[::-1])
-            temp = ''
-
-print(*stack, sep='')
+print(temp[::-1])
+# print(*stack)
+#
+# result = ''
+# for i in stack:
+#     result += i
+# print(result)
