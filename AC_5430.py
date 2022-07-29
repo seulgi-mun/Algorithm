604c09c03a7221a63d34b9748c917e8d2988ddf5
문슬기 <tmfrlghtjr@gmail.com>
2022-07-29 17:48:01 +0900

    add new solution

--- AC_5430.py
+++ AC_5430.py
@@ -35,3 +35,50 @@ for _ in range(T):
         print(arr[::-1])
     else:
         print(arr)
+
+
+"""
+# deque 쓸 때
+import sys
+from collections import deque
+sys.stdin = open('input.txt')
+
+T = int(input())
+
+for _ in range(T):
+    word = input()
+    n = int(input())
+    nums = input()[1:-1].split(',')
+
+    q = deque(nums)
+    flag = 0
+
+    # 빈 배열이면 초기화
+    if n == 0:
+        q = []
+
+    for i in word:
+        if i == 'R':
+            flag += 1
+        elif i == 'D':
+            if len(q) == 0:
+                print('error')
+                break
+            else:
+                # 두번 뒤집은 거면 원래상태니까 그냥 맨 앞에꺼 빼기
+                if flag % 2  == 0:
+                    q.popleft()
+                else:
+                    q.pop()
+
+
+    else:
+        # R이 2번이면 원상태로 출력
+        if flag % 2 == 0:
+            print('[' + ','.join(q) + ']')
+        # 아니면 뒤집기
+        else:
+            q.reverse()
+            print('[' + ','.join(q) + ']')
+            
+"""
