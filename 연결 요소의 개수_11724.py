d3e07972950401c9f21f8d01d159a7c6523dff41
문슬기 <tmfrlghtjr@gmail.com>
2022-09-17 23:31:45 +0900

    upload?

--- 연결 요소의 개수_11724.py
+++ 연결 요소의 개수_11724.py
@@ -1,15 +1,14 @@
 import sys
-sys.stdin = open('input.txt')
-sys.setrecursionlimit(10000)
 from collections import deque
+sys.stdin = open('input.txt')
+
+input = sys.stdin.readline
 
 n, m = map(int, input().split())
 
 graph = [[] for _ in range(n+1)]
-
 visited = [0] * (n+1)
 
-stack = []
 
 def bfs(x):
     global cnt
@@ -17,14 +16,6 @@ def bfs(x):
     # print(q)
     visited[x] = 1
 
-    while q:
-        t = q.popleft()
-
-        for i in graph[t]:
-            # print(i)
-            if visited[i] == 0:
-                visited[i] = 1
-                q.append(i)
 
 
 
@@ -36,14 +27,4 @@ for _ in range(m):
 
 cnt = 0
 
-for j in range(1, n+1):
-    if visited[j] == 0:
-        if not graph[j]:
-            cnt += 1
-            visited[j] = 1
-        else:
-            bfs(j)
-            cnt += 1
 
-print(cnt)
-# print(graph)
