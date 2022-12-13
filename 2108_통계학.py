781696143e57e1db5804d87210729c48ccdeba03
문슬기 <tmfrlghtjr@gmail.com>
2022-12-14 00:10:29 +0900

    algo prac

--- 2108_통계학.py
+++ 2108_통계학.py
@@ -1,10 +1,10 @@
 import sys
 sys.stdin = open('input.txt')
 
+input = sys.stdin.readline
 n = int(input())
 num = [int(input()) for _ in range(n)]
 num.sort()
-# print(num)
 
 tmp = {}
 for i in num:
@@ -12,26 +12,18 @@ for i in num:
         tmp[i] = 1
     else:
         tmp[i] += 1
-set_num = set(num)
 
-preq = []
-for i in set_num:
-    preq.append(num.count(i))
-print(preq.count(max(preq)), '?')
-# print(tmp)
 cnt = []
-# gap = 0
 for k, v in tmp.items():
     cnt.append([k, v])
 cnt.sort(key=lambda x: x[1], reverse=True)
-# print(cnt)
 
 print(round(sum(num)/n))
-print(num[n//2])
+print(num[int(n/2)])
 if n == 1:
     print(*num)
 elif n >= 2:
-    if preq.count(max(preq)) > 2:
+    if cnt[0][1] != cnt[1][1]:
         print(cnt[0][0])
     else:
         print(cnt[1][0])
