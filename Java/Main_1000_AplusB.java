9b334dbc86f31ce0c1ce62e52ae2617d5cb18d0f
문슬기 <tmfrlghtjr@gmail.com>
2022-12-30 03:56:49 +0900

    java prac

---
+++ Java/Main_1000_AplusB.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+
+public class Main_1000_AplusB {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int a = input.nextInt();
+		int b = input.nextInt();
+		System.out.println(a+b);
+		
+		input.close();
+	}
+
+}
