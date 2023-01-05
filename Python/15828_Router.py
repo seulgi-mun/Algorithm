8b871b6be9bdb6143eb1400f70da0056e521d6c4
문슬기 <tmfrlghtjr@gmail.com>
2023-01-05 23:30:45 +0900

    algo prac

---
+++ Python/15828_Router.py
@@ -0,0 +1,29 @@
+import sys
+sys.stdin = open('input.txt')
+from collections import deque
+
+n = int(input())
+
+q = deque()
+
+while True:
+    info = int(input())
+    if len(q) < n:
+        if info == 0:
+            q.popleft()
+        elif info == -1:
+            break
+        else:
+            q.append(info)
+    else:
+        if info == 0:
+            q.popleft()
+    if info == -1:
+        break
+
+if q:
+    print(*q)
+else:
+    print('empty')
+
+    # print(info)
