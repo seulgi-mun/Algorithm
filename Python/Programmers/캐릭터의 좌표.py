dbf986f0ecd5efe8b2b51e11676411ac616d114c
문슬기 <tmfrlghtjr@gmail.com>
2023-03-04 23:43:27 +0900

    algo prac

--- Python/Programmers/캐릭터의 좌표.py
+++ Python/Programmers/캐릭터의 좌표.py
@@ -5,24 +5,13 @@ def solution(keyinput, board):
     se = board[1] // 2
 
     for i in keyinput:
+        if i == 'left' and answer[0] - 1 >= -(ga):
+            answer[0] -= 1
+        elif i == 'right' and answer[0] + 1 <= ga:
+            answer[0] += 1
+        elif i == 'up' and answer[1] + 1 <= se:
+            answer[1] += 1
+        elif i == 'down' and answer[1] - 1 >= -(se):
+            answer[1] -= 1
 
-        if i == 'left':
-            if -board[0] // 2 + 1 < answer[0] - 1 <= board[0] // 2 and -board[1] // 2 + 1 < answer[0] - 1 <= board[
-                1] // 2:
-                answer[0] = answer[0] - 1
-        elif i == 'right':
-            if -board[0] // 2 + 1 < answer[0] + 1 <= board[0] // 2 and -board[1] // 2 + 1 < answer[0] + 1 <= board[
-                1] // 2:
-                answer[0] = answer[0] + 1
-        elif i == 'up':
-
-            if -board[0] // 2 + 1 < answer[1] + 1 <= board[0] // 2 and -board[1] // 2 + 1 < answer[1] + 1 <= board[
-                1] // 2:
-                answer[1] = answer[1] + 1
-        else:
-            if -board[0] // 2 + 1 < answer[1] - 1 <= board[0] // 2 and -board[1] // 2 + 1 < answer[1] - 1 <= board[
-                1] // 2:
-                answer[1] = answer[1] - 1
-    print(board[0] // 2 + 1)
-    print(board[0] // 2 + 1)
     return answer
