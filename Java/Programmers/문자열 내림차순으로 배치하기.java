7e33598ab087893063ebcccc91e494fc7c113872
문슬기 <tmfrlghtjr@gmail.com>
2023-04-25 22:43:20 +0900

    algo prac

---
+++ Java/Programmers/문자열 내림차순으로 배치하기.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+
+        String[] arr = s.split("");
+
+        Arrays.sort(arr, Collections.reverseOrder());
+
+        for (String i : arr) {
+            answer += i;
+        }
+
+        return answer;
+    }
+}
