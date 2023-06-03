30bd822ac2ae9889b135978d3257063126654c8b
문슬기 <tmfrlghtjr@gmail.com>
2023-06-03 22:56:57 +0900

    resol

--- Python/Baekjoon/10811_바구니 뒤집기.py
+++ Python/Baekjoon/10811_바구니 뒤집기.py
@@ -4,3 +4,8 @@ sys.stdin = open('input.txt')
 n, m = map(int, input().split())
 
 ball = list(range(1, n+1))
+
+for _ in range(m):
+    s, e = map(int, input().split())
+    ball = ball[:s-1] + ball[s-1:e][::-1] + ball[e:]
+print(*ball)
